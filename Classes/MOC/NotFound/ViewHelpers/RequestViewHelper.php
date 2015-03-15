6ffbca4d14a4d66bccf9aa8e3f20a64d160342b6
ssh-user <fkdkaske@rodagose.pil.dk>
2015-03-15 19:18:22 +0000

    [TASK] Initial commit

---
+++ Classes/MOC/NotFound/ViewHelpers/RequestViewHelper.php
@@ -0,0 +1,76 @@
+<?php
+namespace MOC\NotFound\ViewHelpers;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Configuration\ConfigurationManager;
+use TYPO3\Flow\Core\Bootstrap;
+use TYPO3\Flow\Http\Request;
+use TYPO3\Flow\Http\RequestHandler;
+use TYPO3\Flow\Http\Response;
+use TYPO3\Flow\Http\Uri;
+use TYPO3\Flow\Mvc\ActionRequest;
+use TYPO3\Flow\Mvc\Routing\Router;
+use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Loads the content of a given URL
+ */
+class RequestViewHelper extends AbstractViewHelper {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Mvc\Dispatcher
+	 */
+	protected $dispatcher;
+
+	/**
+	 * @Flow\Inject(lazy = false)
+	 * @var Bootstrap
+	 */
+	protected $bootstrap;
+
+	/**
+	 * @Flow\Inject(lazy = false)
+	 * @var Router
+	 */
+	protected $router;
+
+	/**
+	 * @Flow\Inject
+	 * @var ConfigurationManager
+	 */
+	protected $configurationManager;
+
+	/**
+	 * Initialize this engine
+	 *
+	 * @return void
+	 */
+	public function initializeObject() {
+		$this->router->setRoutesConfiguration($this->configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_ROUTES));
+	}
+
+	/**
+	 * @param string $path
+	 * @return string
+	 */
+	public function render($path = NULL) {
+		/** @var RequestHandler $activeRequestHandler */
+		$activeRequestHandler = $this->bootstrap->getActiveRequestHandler();
+		$parentHttpRequest = $activeRequestHandler->getHttpRequest();
+		$httpRequest = Request::create(new Uri($parentHttpRequest->getBaseUri() . '/' . $path));
+		$matchingRoute = $this->router->route($httpRequest);
+		if (!$matchingRoute) {
+			throw new Exception(sprintf('Uri with path "%s" could not be found.', $parentHttpRequest->getBaseUri() . '/' . $path), 1426446160);
+		}
+		$request = new ActionRequest($parentHttpRequest);
+		foreach ($matchingRoute as $argumentName => $argumentValue) {
+			$request->setArgument($argumentName, $argumentValue);
+		}
+		$response = new Response($activeRequestHandler->getHttpResponse());
+
+		$this->dispatcher->dispatch($request, $response);
+		return $response->getContent();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+MOC.NotFound
+=============
+
+TYPO3 Neos package that loads a Neos page for displaying a 404 error
+
+Works with TYPO3 Neos 1.0-2.0+
+
+> !!! Not compatible with language dimensions.
+
+Installation
+------------
+composer require "moc/notfound" "1.0.*"
+
+Create a page with the URI segment "404" in the root of your site.
+
+Alternatively set the following configuration in ``Settings.yaml``:
+
+```yaml
+  TYPO3:
+    Flow:
+      error:
+        exceptionHandler:
+          renderingGroups:
+            notFoundExceptions:
+              options:
+                variables:
+                  path: '404'
+```
